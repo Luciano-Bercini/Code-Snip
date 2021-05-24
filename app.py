836961707612c6d47190a3452f23434f6bb261aa
Luciano-Bercini <lucianobercini7@gmail.com>
2021-05-24 19:13:23 +0200

    More improvements.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
 from datetime import *
+from dataclasses import dataclass
 from flask_sqlalchemy import *
 from flask_migrate import Migrate
 
@@ -8,24 +9,26 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db, render_as_batch=True)
 
-
+@dataclass
 class Post(db.Model):
+    id: int
+    title: str
+    content: str
+    language: str
+    date_created: date
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False)
     content = db.Column(db.String(500), nullable=False)
     language = db.Column(db.String(50), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.now(timezone.utc))
 
-    def __repr__(self):
-        return '<Post %r>' % self.id
-
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        post_title = request.form['title']
-        post_content = request.form['content']
-        post_language = request.form['language']
+        post_title = request.form('title')
+        post_content = request.form.get('content')
+        post_language = request.form.get('language')
         new_post = Post(title=post_title, content=post_content, language=post_language)
         try:
             db.session.add(new_post)
@@ -40,9 +43,9 @@ def index():
 def update(id):
     snippet_to_update = Post.query.get_or_404(id)
     if request.method == 'POST':
-        snippet_to_update.title = request.form['title']
-        snippet_to_update.content = request.form['content']
-        snippet_to_update.language = request.form['language']
+        snippet_to_update.title = request.form.get('title')
+        snippet_to_update.content = request.form.get('content')
+        snippet_to_update.language = request.form.get('language')
         try:
             db.session.commit()
             return redirect('/')
@@ -56,6 +59,19 @@ def view(id):
     snippet_to_update = Post.query.get_or_404(id)
     return render_template('view.html', snippet=snippet_to_update)
 
+
+@app.route('/render_query_table', methods=['GET'])
+def render_query_table():
+    post_title = request.args.get('title')
+    post_language = request.args.get('language')
+    conditions = []
+    conditions.append(Post.title.contains(post_title))
+    if post_language != 'Any':
+        conditions.append(Post.language.ilike(post_language))
+    posts = Post.query.filter(*conditions)\
+        .order_by(Post.title.desc())\
+        .all()
+    return jsonify(render_template('query_table.html', posts=posts))
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
--- static/main.js
+++ static/main.js
@@ -13,6 +13,22 @@ function registerServiceWorker() {
         }
     })
 }
+function renderQueryTableInElement(jQueryTableIndex) {
+    $.ajax({
+    type: 'GET',
+    url: '/render_query_table',
+    data: {
+        title: $("#title").val(),
+        language: $("#language").val()
+    },
+    success: (data) => {
+        $(jQueryTableIndex).empty().append(data);
+    },
+    error: function(error) {
+        console.log("Here is the error res: " + JSON.stringify(error));
+    }
+})
+}
 function insertHeader()
 {
     let header = document.createElement("header");
